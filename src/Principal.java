f4fe22448f5c4ca826e93c6e81f87aabcfc4884e
Juan <j.garrigos91@gmail.com>
2022-03-31 09:24:40 +0200

    Primera version

---
+++ src/Principal.java
@@ -0,0 +1,8 @@
+
+public class Principal {
+	public static void main(String[] args) {
+		Coche coche = new Coche("1111AAA",10000);
+		System.out.println(coche);
+		Coche coche2=new Coche("1111BBB",15000);
+	}
+}
