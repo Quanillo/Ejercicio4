40042d48026a984adfc3819340f581b5428c11d2
Juan <j.garrigos91@gmail.com>
2022-03-31 09:47:26 +0200

    Coche 4 añadido

--- src/Principal.java
+++ src/Principal.java
@@ -4,5 +4,11 @@ public class Principal {
 		Coche coche = new Coche("1111AAA",10000);
 		System.out.println(coche);
 		Coche coche2=new Coche("1111BBB",15000);
+		System.out.println(coche2);
+		Coche coche3=new Coche("1111BBB",15000);
+		System.out.println(coche3);
+		Coche coche4=new Coche("1111BBB",15000);
+		System.out.println(coche4);
+		
 	}
 }
